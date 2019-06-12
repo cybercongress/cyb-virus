94c68dbbbc7e23be58b9b636eb4d49539ccd3388
jonybang <Jonybange@gmail.com>
2019-06-12 19:29:52 +0200

    init commit

---
+++ src/popup/router/pages/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>Hello world! 111</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+p {
+  font-size: 20px;
+}
+</style>
